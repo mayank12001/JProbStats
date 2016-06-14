a20bddaa825d5c85c0ac30801dea108462456861
pallavi royal <pallavi.ramicetty@gmail.com>
2016-06-14 14:33:00 +0530

    k-means:max iterations.

---
+++ src/main/java/com/jprobstats/ml/clustering/DoublePoint.java
@@ -0,0 +1,18 @@
+package com.jprobstats.ml.clustering;
+
+import java.io.Serializable;
+
+public class DoublePoint implements Clusterable, Serializable {
+
+    private final double[] points;
+
+    public DoublePoint(double[] points) {
+        this.points = points;
+    }
+
+    @Override
+    public double[] getPoint() {
+        return null;
+    }
+
+}
